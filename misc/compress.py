e7714a62614fb43977179a225b7b4c39e3f76f28
roaris <one0803kyopro@gmail.com>
2022-12-14 23:02:07 +0900

    add misc/compress.py

---
+++ misc/compress.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+
+def compress(l):
+    l = list(set(l))
+    l.sort()
+    idx = defaultdict(int)
+
+    for i in range(len(l)):
+        idx[l[i]] = i
+
+    return idx
